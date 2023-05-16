659428af4e4347930fe8ba4f93f3bc11c6a4ac37
Razvan Gorea <razvan.gorea2@mail.dcu.ie>
2023-05-16 09:00:49 +0100

    Initialized the repo

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+#urls for blog app
+
+#when a path is requested, a view is called to handle the request
+
+#E.g when a user goes to '', the home view is called
+
+urlpatterns = [
+    path('', views.home, name='blog-home'),
+    path('about/', views.about, name='blog-about'),
+]
