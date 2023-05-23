829abec7156c961ba3e5da07083763a92ef59b68
Razvan Gorea <razvan.gorea2@mail.dcu.ie>
2023-05-23 15:00:54 +0100

    Added Post Model into Database

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'Razvan Gorea',
-        'title': 'The Muffin Man',
-        'content': 'Do you know the Muffin Man?',
-        'date_posted': 'May 16, 2023'
-    },
-    {
-        'author': 'Filip Bumbu',
-        'title': 'Star Wars Meme',
-        'content': 'My favorite Star Wars meme is the one with the stormtrooper falling down the stairs.',
-        'date_posted': 'May 17, 2023'
-    }
-]
+from .models import Post
 
 # home view triggered when user goes to an empty path or '/' path
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
