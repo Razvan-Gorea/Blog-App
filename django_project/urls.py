2762b554e330750ceff63e0db1f9e1777e4b1223
Razvan Gorea <razvan.gorea2@mail.dcu.ie>
2023-06-28 14:43:54 +0100

    added the profile url

--- django_project/urls.py
+++ django_project/urls.py
@@ -23,6 +23,7 @@ from users import views as user_views # import views from users app
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'), # when a user goes to the register path, the register view is called
+    path('profile/', user_views.profile, name='profile'), # when a user goes to the register path, the register view is called
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'), # when a user goes to the login path, the login view is called
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'), # when a user goes to the login path, the login view is called
     path('', include('blog.urls')),
